55f5ded881e75f9633b3e893f8be24883a6cde4b
mboichuk <konsyl77@gmail.com>
2018-02-06 11:04:51 +0200

    Added filtering data by type

--- src/App.js
+++ src/App.js
@@ -10,28 +10,52 @@ class App extends Component {
         super();
         this.state = {
             data: []
-        }
+        };
+        this.products = [];
+        this.types = [];
     }
 
     componentDidMount() {
         axios.get('data.json').then((data) => {
+            this.products = data.data;
             this.setState({
-                data: data.data
+                data: this.products
             });
         });
     }
 
+    _toggleProductType = (type) => {
+        let typeIndex = this.types.indexOf(type);
+        if(this.types.indexOf(type)+1){
+            this.types.splice(typeIndex,1)
+        } else {
+            this.types.push(type)
+        }
+        this.types.length ?
+            this.setState({
+                data: this.products.filter((product) => this.types.indexOf(product.type) !== -1)
+            }) :
+            this.setState({data: this.products});
+    };
 
     render() {
         return (
             <div className="App">
                 <div className="app-container">
                     <div className={"row"}>
-                        <div className={"col-2"}>
+                        <div className={"col-2 app-categories"}>
                             <div>
                                 <div className="form-check">
-                                    <input type="checkbox" className="form-check-input" />
+                                    <input type="checkbox" className="form-check-input" onChange = {() => {this._toggleProductType('notebook')}}/>
+                                    <label className="form-check-label" htmlFor="exampleCheck1">Notebooks</label>
+                                </div>
+                                <div className="form-check">
+                                    <input type="checkbox" className="form-check-input" onChange = {() => {this._toggleProductType('tablet')}}/>
                                     <label className="form-check-label" htmlFor="exampleCheck1">Tablets</label>
+                                </div>
+                                <div className="form-check">
+                                    <input type="checkbox" className="form-check-input" onChange = {() => {this._toggleProductType('phone')}}/>
+                                    <label className="form-check-label" htmlFor="exampleCheck1">Mobile Phones</label>
                                 </div>
                             </div>
                         </div>
