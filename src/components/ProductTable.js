67ef3c81a620493363af1b483aca238b8296ac8e
mboichuk <mboichuk@softjourn.com>
2018-02-06 13:08:02 +0200

    Added sorting for table component

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
 
 export default class ProductTable extends Component {
+    constructor() {
+      super();
+      this.state = {
+        products: []
+      };
+      this.sorted = false;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            products: nextProps.products
+        });
+    }
+
+    _sortProductsByCallback = (callback) => {
+      const sorted = this.state.products.sort(callback);
+      this.setState({products: sorted});
+    };
+
+    _sortProducts = (sortBy) => {
+        let callback;
+        switch(sortBy) {
+            case 'id':
+              this.sorted !== 'id' ? callback = (a,b) => a.id > b.id : callback = (a,b) => a.id < b.id;
+              break;
+            case 'name':
+              this.sorted !== 'name' ? callback = (a,b) => a.name.toLowerCase() > b.name.toLowerCase() : callback = (a,b) => a.name.toLowerCase() < b.name.toLowerCase();
+              break;
+            case 'rating':
+              this.sorted !== 'rating' ? callback = (a,b) => a.rating > b.rating : callback = (a,b) => a.rating < b.rating;
+              break;
+            case 'price':
+              this.sorted !== 'price' ? callback = (a,b) => parseInt(a.price.substring(1)) > parseInt(b.price.substring(1))
+                : callback = (a,b) => parseInt(a.price.substring(1)) < parseInt(b.price.substring(1));
+              break;
+            default:
+        }
+        this.sorted !== sortBy ? this.sorted = sortBy : this.sorted = false;
+        this._sortProductsByCallback(callback);
+    }
+
     render() {
         return (
             <table className="table">
                 <thead>
                 <tr>
-                    <th>#</th>
-                    <th>Product Name</th>
-                    <th>Rating</th>
-                    <th>Price</th>
+                    <th onClick={() => {this._sortProducts('id')}}>#</th>
+                    <th onClick={() => {this._sortProducts('name')}}>Product Name</th>
+                    <th onClick={() => {this._sortProducts('rating')}}>Rating</th>
+                    <th onClick={() => {this._sortProducts('price')}}>Price</th>
                 </tr>
                 </thead>
                 <tbody>
-                {this.props.products.map((item, index) =>
+                {this.state.products.map((item, index) =>
                     <tr key={index}>
                         <td>
                             {item.id}
